4b6e0423551bff23ae54ff7c94ef9168fa9d334d
odilovweb <mirzohidodilov49@gmail.com>
2024-08-04 19:02:09 +0500

    netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
